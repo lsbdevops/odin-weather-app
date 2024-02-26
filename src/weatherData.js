d60fc975b32ae6f095c756f671bde3444a470bba
lsbdevops <lsbdevops@gmail.com>
2024-02-26 21:48:53 +1100

    Add html & css layout for weather data

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,10 +1,13 @@
 export default class CurrentWeatherData {
     constructor(data) {
         this.location = data.location;
+        this.time = data.time;
         this.temp = data.temp;
         this.tempFeelsLike = data.feelsLike;
         this.wind = data.wind;
         this.humidity = data.humidity;
         this.uv = data.uv;
+        this.condition = data.condition;
+        this.icon = data.conditionIcon;
     }
 }
--- src/weatherInterface.js
+++ src/weatherInterface.js
@@ -7,11 +7,14 @@ export default function processWeatherData(location) {
             console.log(data);
             const extractedData = {
                 location: data.location.name,
+                time: data.location.localtime,
                 temp: data.current.temp_c,
                 feelsLike: data.current.feelslike_c,
                 wind: data.current.wind_kph,
                 humidity: data.current.humidity,
                 uv: data.current.uv,
+                condition: data.current.condition.text,
+                conditionIcon: data.current.condition.icon,
             };
             return new WeatherStorage(extractedData);
         })
